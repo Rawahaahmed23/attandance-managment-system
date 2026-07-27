6aac3041c0d58b70cef6c63b9334ad03a1e87973
Rawahaahmed23 <rawahaahmed081@gmail.com>
2026-07-28 02:31:47 +0500

    commit

--- client/src/store/useAuth.jsx
+++ client/src/store/useAuth.jsx
@@ -32,7 +32,7 @@ export const UserProvider = ({ children }) => {
       try {
             
         const token = Cookies.get("token");
-        const response = await fetch("https://mern-projects-oj9w.onrender.com/user", {
+        const response = await fetch("https://attandance-managment-system-iv7h.onrender.com/user", {
             method: "GET",
                  credentials: "include",
               headers: {
